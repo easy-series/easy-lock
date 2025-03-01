9e7e9c36b7e0dce478bfd5953f004c85729c7ac0
Enoch <caoyixin.sh@outlook.com>
2025-03-01 20:07:19 +0800

    first commit

---
+++ src/test/java/com/easy/lock/EasyLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
